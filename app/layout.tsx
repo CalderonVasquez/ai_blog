5c320378f4e7f7bf95eb753943c6dd0ff6cdda77
EL Marsho <emarsho@gmail.com>
2023-07-12 12:02:08 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'AI-Boosted Entrepreneur',
+  description: 'We educate small businesses and solopreneurs on all aspects of Artificial Intelligence (AI).',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
