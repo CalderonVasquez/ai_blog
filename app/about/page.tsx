538d0881b5128de260bfbaad8845f135dc753ecf
EL Marsho <emarsho@gmail.com>
2023-07-22 14:27:40 -0500

    added about section

---
+++ app/about/page.tsx
@@ -0,0 +1,54 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>Welcome to The AI-Boosted Entrepreneur!</h1>
+      <p>
+        I'm Eric, a small business owner and entrepreneur who is passionate about using artificial intelligence (AI) to grow and improve my business.
+      </p>
+      <p>
+        I started this blog to share my knowledge and experience with other small businesses and entrepreneurs who are interested in using AI to boost their own businesses.
+      </p>
+
+      <h2>In this blog, you'll find articles on a variety of topics related to AI for small businesses, including:</h2>
+      <ul>
+        <li>How to use AI to automate tasks</li>
+        <li>How to use AI to improve customer service</li>
+        <li>How to use AI to make better marketing decisions</li>
+        <li>How to use AI to increase sales</li>
+        <li>How to use AI to automate social media content</li>
+      </ul>
+
+      <p>
+        Social media is a powerful tool for small businesses to reach new customers, build relationships with existing customers, and promote their products or services. However, it can be time-consuming and challenging to create and share engaging social media content on a regular basis.
+      </p>
+      <p>
+        AI can help small businesses automate social media content by generating text, images, and videos that are tailored to their specific audience. This can free up time for small businesses to focus on other aspects of their business, such as customer service or product development.
+      </p>
+
+      <p>
+        In addition to automating social media content, AI can also help small businesses improve the effectiveness of their social media marketing campaigns. For example, AI can be used to analyze social media data to identify the best times to post, the most effective content formats, and the target audiences that are most likely to engage with their content.
+      </p>
+
+      <p>
+        If you're a small business owner who is interested in using AI to grow your business, then I invite you to subscribe to my blog. I'll be sharing new articles every week, so you'll never miss a thing.
+      </p>
+
+      <p>
+        Thanks for reading!
+      </p>
+
+      <h2>Call to action:</h2>
+      <ul>
+        <li>Subscribe to my blog to get new articles delivered to your inbox every week.</li>
+        <li>Follow me on social media to stay up-to-date on the latest AI trends.</li>
+        <li>Leave a comment below to let me know what you think of my blog.</li>
+      </ul>
+
+      <p>
+        I hope you enjoy my blog!
+      </p>
+    </div>
+  );
+};
+
+export default AboutPage;
