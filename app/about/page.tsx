b5287f21ebe0644a1a9ed24213f071a4c198a3cf
EL Marsho <emarsho@gmail.com>
2023-07-22 14:29:43 -0500

    delete sample code

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,16 +1,17 @@
+
 const AboutPage = () => {
   return (
-    <div>
-      <h1>Welcome to The AI-Boosted Entrepreneur!</h1>
-      <p>
+    <div className="max-w-2xl mx-auto py-8 px-4">
+      <h1 className="text-2xl text-slate-700 font-bold mb-4">Welcome to The AI-Boosted Entrepreneur!</h1>
+      <p className="mb-4">
         I'm Eric, a small business owner and entrepreneur who is passionate about using artificial intelligence (AI) to grow and improve my business.
       </p>
-      <p>
+      <p className="mb-4">
         I started this blog to share my knowledge and experience with other small businesses and entrepreneurs who are interested in using AI to boost their own businesses.
       </p>
 
-      <h2>In this blog, you'll find articles on a variety of topics related to AI for small businesses, including:</h2>
-      <ul>
+      <h2 className="text-xl text-slate-700 font-bold mb-2">In this blog, you'll find articles on a variety of topics related to AI for small businesses, including:</h2>
+      <ul className="list-disc pl-4 mb-4">
         <li>How to use AI to automate tasks</li>
         <li>How to use AI to improve customer service</li>
         <li>How to use AI to make better marketing decisions</li>
@@ -18,33 +19,33 @@ const AboutPage = () => {
         <li>How to use AI to automate social media content</li>
       </ul>
 
-      <p>
+      <p className="mb-4">
         Social media is a powerful tool for small businesses to reach new customers, build relationships with existing customers, and promote their products or services. However, it can be time-consuming and challenging to create and share engaging social media content on a regular basis.
       </p>
-      <p>
+      <p className="mb-4">
         AI can help small businesses automate social media content by generating text, images, and videos that are tailored to their specific audience. This can free up time for small businesses to focus on other aspects of their business, such as customer service or product development.
       </p>
 
-      <p>
+      <p className="mb-4">
         In addition to automating social media content, AI can also help small businesses improve the effectiveness of their social media marketing campaigns. For example, AI can be used to analyze social media data to identify the best times to post, the most effective content formats, and the target audiences that are most likely to engage with their content.
       </p>
 
-      <p>
+      <p className="mb-4">
         If you're a small business owner who is interested in using AI to grow your business, then I invite you to subscribe to my blog. I'll be sharing new articles every week, so you'll never miss a thing.
       </p>
 
-      <p>
+      <p className="mb-4">
         Thanks for reading!
       </p>
 
-      <h2>Call to action:</h2>
-      <ul>
+      <h2 className="text-xl text-slate-700 font-bold mb-2">Call to action:</h2>
+      <ul className="list-disc pl-4 mb-4">
         <li>Subscribe to my blog to get new articles delivered to your inbox every week.</li>
         <li>Follow me on social media to stay up-to-date on the latest AI trends.</li>
         <li>Leave a comment below to let me know what you think of my blog.</li>
       </ul>
 
-      <p>
+      <p className="mb-4">
         I hope you enjoy my blog!
       </p>
     </div>
