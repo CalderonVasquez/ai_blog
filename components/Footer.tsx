c6dfa188f047666e523e19354756d5e9f0e73369
EL Marsho <emarsho@gmail.com>
2023-07-24 22:36:04 -0500

    added mailchimp subscribe

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,16 @@
+import MailchimpSubscribeForm from "./MailchimpSubscriberForm";
+
 const Footer: React.FC = () => {
   return (
     <footer>
+      <div className="text-slate-700">
+        <MailchimpSubscribeForm />
+      </div>
       <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-500">
         <h2>AI-Boosted Entrepreneur</h2>
         <p className="mb-1">Copyright © 2023 AI-Boosted Entrepreneur</p>
-        <form>
-          <input type="email" placeholder="Enter your email" className="w-64 py-2 px-4 border-slate-400 rounded-md" />
-          <button type="submit" className="bg-gradient-to-br from-slate-900 via-slate-800 to-slate-700 text-white font-bold py-2 px-3 rounded-md">
-            Subscribe
-          </button>
-        </form>
       </div>
+      
     </footer>
   );
 };
